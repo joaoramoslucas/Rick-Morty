385fadda2a76f1391a0ae7ef756492832ebfa0cc
Jao <joaopedroramosdelucas10@gmail.com>
2024-08-28 14:23:28 -0300

    Ajuste interno

--- Rick/View/CharacterDetail.swift
+++ Rick/View/CharacterDetail.swift
@@ -1,60 +1,67 @@
+//
+//  RickApp.swift
+//  Rick
+//
+//  Created by Jao on 21/08/24.
+//
+
 import SwiftUI
 
 struct CharacterDetail: View {
-    var character: Character
+    var character: Character // Propriedade para armazenar o personagem atual a ser exibido
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 30) { // Aumenta o espaçamento entre os elementos
-                // Centralizar o conteúdo
+        ScrollView { // Permite que o conteúdo role verticalmente se necessário
+            VStack(spacing: 30) { // Empilha os elementos verticalmente com um espaçamento maior entre eles
+                // Centraliza o conteúdo dentro de uma VStack
                 VStack {
-                    // Imagem do personagem com borda e sombra
+                    // Verifica se a URL da imagem é válida
                     if let imageUrl = character.image, let url = URL(string: imageUrl) {
-                        AsyncImage(url: url) { image in
+                        AsyncImage(url: url) { image in // Carrega a imagem de forma assíncrona
                             image
                                 .resizable()
-                                .scaledToFit()
-                                .frame(maxHeight: 250)
-                                .clipShape(RoundedRectangle(cornerRadius: 15))
-                                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.gray, lineWidth: 2))
-                                .shadow(radius: 5)
+                                .scaledToFit() // Ajusta a imagem mantendo a proporção
+                                .frame(maxHeight: 250) // Limita a altura máxima da imagem
+                                .clipShape(RoundedRectangle(cornerRadius: 15)) // Aplica um corte arredondado na imagem
+                                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.gray, lineWidth: 2)) // Adiciona uma borda cinza
+                                .shadow(radius: 5) // Aplica uma sombra à imagem para destacá-la
                         } placeholder: {
-                            ProgressView()
+                            ProgressView() // Exibe um indicador de progresso enquanto a imagem carrega
                         }
-                        .padding(.bottom, 20) // Aumenta o espaçamento abaixo da imagem
+                        .padding(.bottom, 20) // Adiciona espaçamento abaixo da imagem para separação visual
                     }
-                    
-                    // Nome do personagem com estilo aprimorado
+
+                    // Exibe o nome do personagem com estilo de fonte grande e negrito
                     Text(character.name)
                         .font(.largeTitle)
                         .fontWeight(.bold)
-                        .foregroundColor(.primary)
-                        .padding(.bottom, 5)
+                        .foregroundColor(.primary) // Cor do texto primária que adapta ao modo escuro/claro
+                        .padding(.bottom, 5) // Adiciona espaçamento abaixo do nome
                 }
                 
-                // Detalhes do personagem centralizados com borda e fundo
-                VStack(alignment: .leading, spacing: 15) {
-                    Text("Status: \(character.status ?? "Desconhecido")")
-                    Text("Espécie: \(character.specie ?? "Desconhecido")")
-                    Text("Gênero: \(character.gender)")
-                    Text("Origem: \(character.origin.name)")
+                // Exibe os detalhes do personagem em uma caixa com borda e fundo
+                VStack(alignment: .leading, spacing: 15) { // Alinhamento à esquerda com espaçamento aumentado
+                    Text("Status: \(character.status ?? "Desconhecido")") // Exibe o status do personagem ou "Desconhecido" se for nulo
+                    Text("Espécie: \(character.specie ?? "Desconhecido")") // Exibe a espécie do personagem ou "Desconhecido"
+                    Text("Gênero: \(character.gender)") // Exibe o gênero do personagem
+                    Text("Origem: \(character.origin.name)") // Exibe a origem do personagem
                 }
-                .font(.title3)
-                .foregroundColor(.secondary)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-                .frame(maxWidth: .infinity) // Para ocupar toda a largura possível
-                .shadow(radius: 5) // Adiciona sombra para destacar a caixa de detalhes
+                .font(.title3) // Define o estilo de fonte para os detalhes
+                .foregroundColor(.secondary) // Define uma cor secundária para o texto
+                .padding() // Adiciona preenchimento ao redor da caixa de detalhes
+                .background(Color(.systemGray6)) // Define um fundo cinza claro para a caixa de detalhes
+                .cornerRadius(10) // Arredonda os cantos da caixa de detalhes
+                .frame(maxWidth: .infinity) // Permite que a caixa de detalhes ocupe toda a largura disponível
+                .shadow(radius: 5) // Adiciona uma sombra à caixa de detalhes para destaque visual
                 
-                Spacer()
+                Spacer() // Empurra o conteúdo para o topo, deixando espaço no final
             }
-            .padding(.horizontal)
-            .frame(maxWidth: .infinity, alignment: .center) // Centraliza todo o conteúdo
-            .background(Color(.systemBackground))
+            .padding(.horizontal) // Adiciona preenchimento horizontal ao conteúdo da ScrollView
+            .frame(maxWidth: .infinity, alignment: .center) // Centraliza todo o conteúdo horizontalmente
+            .background(Color(.systemBackground)) // Define o fundo da ScrollView conforme o tema do sistema
         }
-        .navigationTitle("Detalhes do Personagem") // Adiciona um título à tela
-        .navigationBarTitleDisplayMode(.inline) // Exibe o título no modo compacto
+        .navigationTitle("Detalhes do Personagem") // Adiciona um título à barra de navegação
+        .navigationBarTitleDisplayMode(.inline) // Exibe o título da barra de navegação no modo compacto
     }
 }
 
@@ -74,7 +81,7 @@ struct CharacterDetail_Previews: PreviewProvider {
             created: ""
         )
         CharacterDetail(character: exampleCharacter)
-            .preferredColorScheme(.light) // Adiciona visualização em modo claro
-            .previewLayout(.sizeThatFits) // Ajusta o layout da pré-visualização
+            .preferredColorScheme(.light) // Adiciona visualização em modo claro para a prévia
+            .previewLayout(.sizeThatFits) // Ajusta o layout da pré-visualização para caber no tamanho de conteúdo
     }
 }
--- Rick/View/Home.swift
+++ Rick/View/Home.swift
@@ -1,100 +1,105 @@
 //
-//  Home.swift
+//  RickApp.swift
 //  Rick
 //
 //  Created by Jao on 21/08/24.
 //
+
 import SwiftUI
 
 struct Home: View {
-    @ObservedObject var viewModel = HomeViewModel()
-    
+    @ObservedObject var viewModel = HomeViewModel() // Observa mudanças no ViewModel para atualizar a UI
+
+    // Configuração das colunas para o LazyVGrid
     private let columns = [
-        GridItem(.flexible(), spacing: 16),
-        GridItem(.flexible(), spacing: 16)
+        GridItem(.flexible(), spacing: 16), // Coluna flexível com espaçamento de 16 pontos
+        GridItem(.flexible(), spacing: 16)  // Outra coluna flexível com o mesmo espaçamento
     ]
     
-    @State private var pulsate = false
-    
+    @State private var pulsate = false // Estado para controlar o efeito de pulsação
+
     var body: some View {
-        NavigationView {
-            Group {
+        NavigationView { // Cria uma view de navegação
+            Group { // Agrupa diferentes estados de exibição
                 if viewModel.isLoading {
+                    // Mostra um indicador de progresso quando os dados estão carregando
                     ProgressView("Carregando personagens...")
                         .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity) // Centraliza o indicador
                 } else if viewModel.onErr {
+                    // Exibe uma mensagem de erro se ocorrer um problema ao carregar os personagens
                     Text("Erro ao carregar personagens.")
                         .foregroundColor(.red)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity) // Centraliza a mensagem
                         .padding()
                 } else {
-                    ScrollView {
-                        LazyVGrid(columns: columns, spacing: 16) {
-                            ForEach(viewModel.characters) { char in
-                                NavigationLink(destination: CharacterDetail(character: char)) {
-                                    VStack {
+                    // Exibe os personagens em uma grade quando os dados são carregados com sucesso
+                    ScrollView { // Permite rolar o conteúdo verticalmente
+                        LazyVGrid(columns: columns, spacing: 16) { // Cria uma grade flexível de duas colunas
+                            ForEach(viewModel.characters) { char in // Itera sobre a lista de personagens
+                                NavigationLink(destination: CharacterDetail(character: char)) { // Link para a tela de detalhes do personagem
+                                    VStack { // Empilha verticalmente a imagem e o nome do personagem
                                         if let imageUrl = char.image, let url = URL(string: imageUrl) {
-                                            AsyncImage(url: url) { image in
+                                            AsyncImage(url: url) { image in // Carrega a imagem de forma assíncrona
                                                 image
                                                     .resizable()
                                                     .scaledToFill()
-                                                    .frame(width: 150, height: 150)
-                                                    .clipped()
-                                                    .modifier(PulsatingEffect())
+                                                    .frame(width: 150, height: 150) // Define o tamanho da imagem
+                                                    .clipped() // Corta a imagem para ajustar ao frame
+                                                    .modifier(PulsatingEffect()) // Aplica o efeito de pulsação
                                             } placeholder: {
-                                                ProgressView()
+                                                ProgressView() // Indicador de progresso enquanto a imagem carrega
                                                     .frame(width: 150, height: 150)
                                                     .background(Color.gray.opacity(0.2))
-                                                    .cornerRadius(10)
+                                                    .cornerRadius(10) // Borda arredondada para o placeholder
                                             }
                                         } else {
-                                            Rectangle()
+                                            Rectangle() // Exibe um retângulo cinza caso a imagem não esteja disponível
                                                 .fill(Color.gray)
                                                 .frame(width: 150, height: 150)
                                                 .cornerRadius(10)
                                         }
-                                        Text(char.name)
+                                        Text(char.name) // Exibe o nome do personagem
                                             .font(.headline)
                                             .multilineTextAlignment(.center)
                                             .padding(.horizontal, 8)
                                             .padding(.vertical, 4)
-                                            .background(Color.white.opacity(0.7))
+                                            .background(Color.white.opacity(0.7)) // Fundo semitransparente para o texto
                                             .cornerRadius(4)
                                     }
                                     .frame(maxWidth: .infinity)
-                                    .background(Color.gray.opacity(0.1))
-                                    .cornerRadius(10)
-                                    .shadow(radius: 5)
+                                    .background(Color.gray.opacity(0.1)) // Fundo cinza claro para a célula do personagem
+                                    .cornerRadius(10) // Borda arredondada para a célula do personagem
+                                    .shadow(radius: 5) // Sombra para a célula do personagem
                                 }
                             }
                         }
-                        .padding()
+                        .padding() // Adiciona espaçamento ao redor da grade
                     }
                 }
             }
-            .navigationTitle("Personagens")
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Personagens") // Título da barra de navegação
+            .navigationBarTitleDisplayMode(.inline) // Exibe o título no modo compacto
             .onAppear {
-                viewModel.getCharacters()
+                viewModel.getCharacters() // Chama o método para carregar os personagens ao aparecer a view
             }
         }
     }
 }
 
-struct PulsatingEffect: ViewModifier {
-    @State private var pulsate = false
+struct PulsatingEffect: ViewModifier { // Modificador de view para o efeito de pulsação
+    @State private var pulsate = false // Estado para controlar a animação de pulsação
 
     func body(content: Content) -> some View {
         content
-            .scaleEffect(pulsate ? 1.0 : 1.1)
+            .scaleEffect(pulsate ? 1.0 : 1.1) // Alterna o tamanho da view para criar o efeito de pulsação
             .animation(
-                Animation.easeInOut(duration: 2.5)
-                    .repeatForever(autoreverses: true),
+                Animation.easeInOut(duration: 2.5) // Animação suave que dura 2.5 segundos
+                    .repeatForever(autoreverses: true), // Repetição infinita da animação, revertendo a escala
                 value: pulsate
             )
             .onAppear {
-                pulsate = true
+                pulsate = true // Ativa a animação ao aparecer a view
             }
     }
 }
@@ -102,6 +107,6 @@ struct PulsatingEffect: ViewModifier {
 // Preview da tela Home
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        Home() // Exibe a preview da tela Home
     }
 }
